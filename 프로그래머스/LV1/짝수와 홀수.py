de33b0a4cac4cad213d919eaea1049d7625f6894
laits1 <thsehdrl1@gmail.com>
2021-08-03 21:40:25 +0900

    2021년 08월  3일 화 오후  9:40:25

---
+++ 프로그래머스/LV1/짝수와 홀수.py
@@ -0,0 +1,11 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12937
+
+def solution(num):
+    answer = "Odd"
+    if num % 2 == 0 :
+        answer = "Even"   
+    return answer
+
+
+# def evenOrOdd(num):
+#     return "Even" if num%2 == 0 else "Odd"
